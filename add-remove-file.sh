e9a56bb927913c6bb126ad72457d76a2c3fb074d
iamrobinhood12345 <bshields23@gmail.com>
2022-06-06 20:02:35 -0700

    commit update

---
+++ add-remove-file.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+DIRECTORY='dir'
+
+
+echo creating a file
+touch ${DIRECTORY}/hey.txt
+echo adding file
+git -C "$DIRECTORY" add .
+echo committing file
+git -C "$DIRECTORY" commit -m 'add hey.txt'
+echo pushing file to origin
+git -C "$DIRECTORY" push origin -u main
+echo removing file
+rm -rf ${DIRECTORY}/hey.txt
+echo adding file removal
+git -C "$DIRECTORY" add .
+echo committing file removal
+git -C "$DIRECTORY" commit -m 'remove hey.txt'
+echo pushing file removal to origin
+git -C "$DIRECTORY" push origin -u main
+
+git -C "$DIRECTORY" status
